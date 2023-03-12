7bccf789f8c80d4bd5ef166e3d8f872186f8ccfe
Mustafa4270 <117478606+Mustafa4270@users.noreply.github.com>
2023-03-12 12:24:14 +0300

    Add files via upload
    PYTHON & SELENIUM KAMP 2023 ÖDEV 1

---
+++ PythonOdev1.py
@@ -0,0 +1,58 @@
+#Veri tipleriyle örnek tanımlama
+
+#str veri tipiyle atanmıştır
+x = ("Merhaba Python")
+
+#int veri tipiyle atanmıştır
+y = (50)
+
+#float veri tipiyle atanmıştır
+z = (17.5)
+
+#complex veri tipiyle atanmıştır
+t = (7j)
+
+#list veri tipiyle atanmıştır
+a = ["ağaç","yaprak","solucan"]
+
+#tuple veri tipiyle atanmıştır
+b = ("elma","armut","portakal")
+
+#range veri tipiyle atanmıştır
+c = range(6)
+
+#dict veri tipiyle atanmıştır
+d = {"name" : John, "age" : 36}
+
+#set veri tipiyle atanmıştır
+e = {"apple", "banana", "cherry"}
+
+#frozenset veri tipiyle atanmıştır
+f = frozenset({"apple", "banana", "cherry"})
+
+#bool veri tipiyle atanmıştır
+g = True
+
+#bytes veri tipiyle atanmıştır
+h = b"Hello"
+
+#bytearray veri tipiyle atanmıştır
+k = bytearray(5)
+
+#memoriview veri tipiyle atanmıştır
+l = memoryview(bytes(5))
+
+#nonetype veri tipiyle atanmıştır
+m = none 
+
+
+
+
+#Kodlama.io'da kullanıldığını düşündüğüm şart blokları
+
+OdevTamamlamaYuzdesi = a
+a = 100
+GerekenTamamlamaYuzdesi = b
+b = 100
+if a >= b:
+ print = ("Ödev Tamamlandı")
